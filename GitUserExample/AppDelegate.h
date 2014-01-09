eefb68bea151966bef3deb34b74e12bf9b8f1f1f
lynn <lynn.coglink@gmail.com>
2014-01-09 13:07:19 +0800

    Initial Commit

---
+++ GitUserExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUserExample
+//
+//  Created by Lynn on 14-1-9.
+//  Copyright (c) 2014年 Lynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUserExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUserExample
+//
+//  Created by Lynn on 14-1-9.
+//  Copyright (c) 2014年 Lynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
