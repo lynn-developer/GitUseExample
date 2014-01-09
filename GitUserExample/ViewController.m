ce18f8ea8e2a48ae01951089aac2ae811191937e
lynn <lynn.coglink@gmail.com>
2014-01-09 14:02:06 +0800

    delete default method

--- GitUserExample/ViewController.m
+++ GitUserExample/ViewController.m
@@ -19,16 +19,4 @@
     NSLog(@"This is a git test");
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
