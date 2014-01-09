a796ceb315888b8446b6d313cca98c98a9f62c45
lynn <lynn.coglink@gmail.com>
2014-01-09 13:46:30 +0800

    Creats new classed for map itinerary feature

---
+++ GitUserExample/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitUserExample
+//
+//  Created by Lynn on 14-1-9.
+//  Copyright (c) 2014年 Lynn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
---
+++ GitUserExample/MapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.m
+//  GitUserExample
+//
+//  Created by Lynn on 14-1-9.
+//  Copyright (c) 2014年 Lynn. All rights reserved.
+//
+
+#import "MapForItinerary.h"
+
+@implementation MapForItinerary
+
+@end
